eeabb9fb2ce4495f3da68ce5f4d3ddda8bf5d392
alexkimeu0 <kim3ual3x@gmail.com>
2020-11-26 14:14:06 +0300

    Added files for Heroku deploy

--- course/settings.py
+++ course/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,6 +73,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'course.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- predictapp/views.py
+++ predictapp/views.py
@@ -26,7 +26,7 @@ def database(request):
 
 def predict(request):
     if request.method == 'POST':
-        gender = int(request.POST.get('gender'))   
+        gender = request.POST.get('gender')   
         grade = request.POST.get('grade')
         openness = int(request.POST.get('openness'))
         agreeableness = int(request.POST.get('agreeableness'))
@@ -60,11 +60,13 @@ def predict(request):
             grade = 20
         else:
             grade = 10
+      
 
         # Load the model & unpickle
         with open('/home/code/Projects/ML/Course/courses_pickle', 'rb') as f:
             md = pickle.load(f)
     
+
             # Predict & return the results   
             course = md.predict([ [gender, grade, openness, agreeableness, emotions, conscientiousness, extraversion] ])[0] #Pass the values from the form
         
