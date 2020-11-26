c909b638f8885cb9ceb8f9bb5ac16cc946d9f5f1
alexkimeu0 <kim3ual3x@gmail.com>
2020-11-26 15:10:55 +0300

    cleaned up

--- course/settings.py
+++ course/settings.py
@@ -126,4 +126,3 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- course/wsgi.py
+++ course/wsgi.py
@@ -8,12 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'course.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-
